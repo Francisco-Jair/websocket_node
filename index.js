5de55bd6990d1627c8b91c1f0de3f41bdc57607d
Francisco Jair <fjair123@ufpi.edu.br>
2022-01-29 08:45:35 -0300

    Implementando uma aplicação websocket

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (requisicao, resposta) => {
+  resposta.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("Alguem se conectou com o id " + socket.id);
+});
+
+function geravalor() {
+  return (Math.random() * 100).toFixed(2);
+}
+
+setInterval(() => {
+  io.emit("cotação", geravalor());
+}, 1000);
+
+server.listen(3000, () => {
+  console.log("Ouvindo na porta 3000");
+});
